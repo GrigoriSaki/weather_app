301bff467408a73e40d7629df65b9ae322b9d0ab
Grzegorz Wisniowski <grzesiekwis200@gmail.com>
2025-04-21 19:58:14 +0200

    notify schedule to finish

--- lib/components/services/noti_service.dart
+++ lib/components/services/noti_service.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:flutter_timezone/flutter_timezone.dart';
 
 class NotiService {
   final notificationsPlugin = FlutterLocalNotificationsPlugin();
@@ -28,7 +30,7 @@ class NotiService {
 
   //Notification details
 
-  NotificationDetails notificationDetails() {
+  NotificationDetails notificationDetailsForAndroid() {
     return const NotificationDetails(
         android: AndroidNotificationDetails(
             'basic_channel', 'Daily Notifications',
@@ -41,6 +43,32 @@ class NotiService {
   Future<void> showNotification(
       {int id = 0, String? title, String? body}) async {
     return await notificationsPlugin.show(
-        id, title, body, NotificationDetails());
+      id,
+      title,
+      body,
+      notificationDetailsForAndroid(),
+    );
+  }
+
+//schedule notification
+  Future<void> scheduleNotification({
+    int id = 1,
+    required String title,
+    required String body,
+    required int hour,
+    required int minute,
+  }) async {
+    //get the current time in device's local timezone
+
+    final now = tz.TZDateTime.now(tz.local);
+
+    //create the date and time for today at specified hour and minute
+    var scheduledDate =
+        tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, minute);
+
+    //scheduled the notification
+    await notificationsPlugin.zonedSchedule(
+        id, title, body, scheduledDate, NotificationDetails(),
+        androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle);
   }
 }
